b419f9fca5bafc64313c285357c247b27f970cac
Caroliny S Q G Cagnoni <cgcagnoni@gmail.com>
2022-07-19 16:28:01 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoCWebAPI.Models;
+
+namespace PoCWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        private DbEscola dbEscola = new DbEscola();
+
+        [HttpGet]
+        public List<Aluno> AcessarAlunos()
+        {
+            return dbEscola.Alunos.ToList();
+        }
+
+        [HttpGet("{Id}")]
+        public Aluno AcessarAlunoPelaId(int Id)
+        {
+            Aluno Resultado = new Aluno();
+            foreach (Aluno aluno in dbEscola.Alunos)
+            {
+                if (aluno.Id == Id)
+                {
+                    Resultado = aluno;
+                }
+            }
+            return Resultado;
+        }
+
+        [HttpPost]
+        public virtual Aluno CadastrarAluno(Aluno novoAluno)
+        {
+            dbEscola.Alunos.Add(novoAluno);
+            dbEscola.SaveChanges();
+            
+            return novoAluno;
+        }
+
+        [HttpDelete("{Id}")]
+        public Aluno DeletarAluno(int Id)
+        {
+            Aluno AlunoDeletado = new Aluno();
+            foreach (Aluno aluno in dbEscola.Alunos)
+            {
+                if (aluno.Id == Id)
+                {
+                    AlunoDeletado = aluno;
+                    dbEscola.Alunos.Remove(AlunoDeletado);
+                    dbEscola.SaveChanges();
+                }
+            }
+            return AlunoDeletado;
+           
+        } 
+
+    }
+}
---
+++ Controllers/DbEscola.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PoCWebAPI.Models
+{
+    public class DbEscola : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder Configuracao)
+        {
+            Configuracao.UseInMemoryDatabase("Escola");
+        }
+    }
+}
